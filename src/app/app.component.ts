95ab8a1c8677fa15754d18adcd9319394da377ca
Fabiano <fabiano.ssantos79@gmail.com>
2024-08-25 23:34:40 -0300

    feat: insert components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from "./components/container/container.component";
+import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
+import { SeparadorComponent } from "./components/separador/separador.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent, SeparadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstwuvxyz';
 }
